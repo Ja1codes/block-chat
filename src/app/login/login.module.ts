e1fd5f2419d424719c4ce0c302166b708a61039f
Rahul Bhat <bhatrahul801@gmail.com>
2023-05-01 20:31:53 +0530

    login page

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginPageComponent } from './login-page/login-page.component';
+import { SignupPageComponent } from './signup-page/signup-page.component';
 
 @NgModule({
   declarations: [
     LoginPageComponent,
+    SignupPageComponent,
   ],
   imports: [
     CommonModule
